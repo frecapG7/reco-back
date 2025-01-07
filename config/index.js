153175ff0ed23ab27cacb8548b144e464b67c5da
frecape <florian.recape@wanadoo.fr>
2025-01-07 09:38:29 +0100

    Config made clean

--- config/index.js
+++ config/index.js
@@ -4,6 +4,12 @@ const config = {
   TOKEN_SECRET: process.env.TOKEN_SECRET,
   GCLOUD_STORAGE_BUCKET: process.env.GCLOUD_STORAGE_BUCKET,
   GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+
+  db: {
+    uri: process.env.MONGODB_URI,
+    user: process.env.MONGODB_USER,
+    password: process.env.MONGODB_PASSWORD,
+  },
 };
 
 module.exports = config;
--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
+const config = require("./config");
 
 mongoose.set("transactionAsyncLocalStorage", true);
 // mongoose.set("debug", true);
 
+const buildMongoUri = () => {
+  const { uri, user, password } = config.db;
+  return uri.replace("<username>", user).replace("<password>", password);
+};
+
 connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(buildMongoUri(), {
       serverApi: { version: "1", strict: true, deprecationErrors: true },
     });
     console.log("Connected to MongoDB...");
