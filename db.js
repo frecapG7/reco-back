3c39ca31fe54d08cd5bd02fc07dd8b762a1abb5d
frecape <florian.recape@wanadoo.fr>
2025-01-07 16:29:31 +0100

    New condif
    For the worst lol

--- db.js
+++ db.js
@@ -1,31 +1,16 @@
 const mongoose = require("mongoose");
 const config = require("./config");
 
-mongoose.set("transactionAsyncLocalStorage", true);
-// mongoose.set("debug", true);
+// mongoose.set("transactionAsyncLocalStorage", true);
+mongoose.set("debug", true);
 
 const buildMongoUri = () => {
   const { uri, user, password } = config.db;
   return uri.replace("<username>", user).replace("<password>", password);
 };
 
-connect = async () => {
-  try {
-    await mongoose.connect(buildMongoUri(), {
-      serverApi: { version: "1", strict: true, deprecationErrors: true },
-    });
-    console.log("Connected to MongoDB...");
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
-mongoose.connection.on("disconnected", () => {
-  console.log("MongoDB disconnected");
+mongoose.connect(buildMongoUri(), {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
 });
 
 module.exports = mongoose;
